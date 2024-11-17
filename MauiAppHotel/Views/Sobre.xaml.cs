91200e140c50f595c9e92a0f9686575622c3a9e0
erika <eaq990@gmail.com>
2024-11-17 14:37:49 -0300

    Modificando o layout das telas

---
+++ MauiAppHotel/Views/Sobre.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiAppHotel.Views;
+
+public partial class Sobre : ContentPage
+{
+    public Sobre()
+    {
+        InitializeComponent();
+    }
+
+    private async void OnBackButtonClicked(object sender, EventArgs e)
+    {
+        // Navega de volta para a página anterior
+        await Navigation.PopAsync();
+    }
+}
